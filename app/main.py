70f721acc1417c518cb5b4fa3aceba6b58270893
Rickymac123 <ricky@rickymc.co.uk>
2026-02-23 02:10:16 +0000

    Include reviews router in app.main and fix cron endpoint

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ from app.routers.dashboards import router as dashboards_router
 from app.routers.admin import router as admin_router
 from app.routers.uploads import router as uploads_router
 from app.routers.availability import router as availability_router
+from app.routers.reviews import router as reviews_router
 
 app = FastAPI(title="ContractPros API")
 
@@ -62,6 +63,7 @@ app.include_router(dashboards_router)
 app.include_router(admin_router)
 app.include_router(uploads_router)
 app.include_router(availability_router)
+app.include_router(reviews_router)
 
 @app.on_event("startup")
 def on_startup():
